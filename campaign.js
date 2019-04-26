e09cc123c602a8827c88c9d949ad82044c711aee
Keith Mattison <klmattis@ncsu.edu>
2019-04-26 19:12:21 -0400

    refactor of campaign

--- campaign.js
+++ campaign.js
@@ -1,4 +1,4 @@
-import web3 from './web3';
+import web3 from '../web3';
 import Campaign from './build/Campaign.json';
 
 export default (address) => {
--- factory.js
+++ factory.js
@@ -1,4 +1,4 @@
-import web3 from './web3';
+import web3 from '../web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
 const _ADDRESS = process.env.CONTRACT_ADDRESS;
