7312d87c8d92acab5601ab0fb1b2507a5318af64
Keith Mattison <klmattis@ncsu.edu>
2019-04-27 09:06:18 -0400

    switched double quotes for single quotes for consistency

--- factory.js
+++ factory.js
@@ -5,7 +5,7 @@ const _ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const instance = new web3.eth.Contract(
 	JSON.parse(CampaignFactory.interface),
-	"0x87132721003770C7be355481789db08B9Fa07020"
+	'0x87132721003770C7be355481789db08B9Fa07020'
 );
 
 export default instance;
