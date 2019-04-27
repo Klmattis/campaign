7d46fc5517120a0fe69424dbf7ee31dc58418988
Keith Mattison <klmattis@ncsu.edu>
2019-04-27 12:32:23 -0400

    fixing imports and abi reference

--- campaign.js
+++ campaign.js
@@ -1,9 +1,9 @@
 import web3 from '../web3';
-import Campaign from './build/Campaign.json';
+import Campaign from './build/contracts/Campaign.json';
 
 export default (address) => {
 	return new web3.eth.Contract(
-		JSON.parse(Campaign.interface), 
+		Campaign.abi, 
 		address
 	);
 };
--- factory.js
+++ factory.js
@@ -1,10 +1,10 @@
 import web3 from '../web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import CampaignFactory from './build/contracts/CampaignFactory.json';
 
 const _ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const instance = new web3.eth.Contract(
-	JSON.parse(CampaignFactory.interface),
+	CampaignFactory.abi,
 	'0x87132721003770C7be355481789db08B9Fa07020'
 );
 
