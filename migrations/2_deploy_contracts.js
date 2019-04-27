c4983691d18cb59b9dfafb33166f262155271842
Keith Mattison <klmattis@ncsu.edu>
2019-04-27 12:42:11 -0400

    removed unneeded const

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var CampaignFactory = artifacts.require("CampaignFactory");
 
 module.exports = async function(deployer) {
-    const deployedCampaignFactory = await deployer.deploy(CampaignFactory).then(() => console.log("Contract deployed to: " + CampaignFactory.address));
+    await deployer.deploy(CampaignFactory).then(() => console.log("Contract deployed to: " + CampaignFactory.address));
 };
